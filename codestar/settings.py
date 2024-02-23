5ab9a5ddeeb73fd097f08120810d94b5507cfb0f
sharon simpson <sharon.m.simpson@icloud.com>
2024-02-23 13:58:55 +0000

    added a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -90,6 +90,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-sharnm-blogdjango-hsynxv8ycgu.ws-eu108.gitpod.io",
+    "https://django-blog04-b9d72fd07af1.herokuapp.com"
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
